97f3200818cd3c3ea2dd5a70d1a3929fcf90015e
Treecase <trevorclast@gmail.com>
2026-02-27 15:53:35 -0500

    fix: include wad headers in wad.h

--- wad/wad.h
+++ wad/wad.h
@@ -3,7 +3,15 @@
 
 #define __WAD_H_INSIDE__
 
+#include <wad/wad-directoryentry.h>
+#include <wad/wad-fontfile.h>
+#include <wad/wad-inputstream.h>
+#include <wad/wad-loaderror.h>
+#include <wad/wad-miptexfile.h>
+#include <wad/wad-qpicfile.h>
+#include <wad/wad-rgb.h>
 #include <wad/wad-root.h>
+#include <wad/wad-texturearchive.h>
 
 #undef __WAD_H_INSIDE__
 
